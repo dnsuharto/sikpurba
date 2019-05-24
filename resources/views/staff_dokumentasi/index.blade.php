3344ac2c5295a72043161302398bdb62839d963f
Gerry Herdianto <gerry.herdianto@gmail.com>
2019-05-24 16:35:38 +0700

    ok

--- resources/views/staff_dokumentasi/index.blade.php
+++ resources/views/staff_dokumentasi/index.blade.php
@@ -4,62 +4,68 @@
 
 <section class="content">
 <div class="row">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
+
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-purple"><i class="ion ion-ios-paw-outline"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Jumlah Total Koleksi</span>
+                <span class="info-box-number">{{ number_format($jumlah_total_koleksi, 0, '.', ',') }}</span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="ion ion-ios-paw-outline"></i></span>
-
             <div class="info-box-content">
-              <span class="info-box-text">Vertebrata & Hominid</span>
-              <span class="info-box-number">90<small>%</small></span>
+                <span class="info-box-text">Vertebrata & Hominid</span>
+                <span class="info-box-number">{{ number_format($jumlah_vertebrata, 0, '.', ',') }}</span>
             </div>
             <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
             <span class="info-box-icon bg-red"><i class="ion-ios-paw-outline"></i></span>
-
             <div class="info-box-content">
-              <span class="info-box-text">Batuan & Mineral</span>
-              <span class="info-box-number">41,410</span>
+                <span class="info-box-text">Batuan & Mineral</span>
+                <span class="info-box-number">41,410</span>
             </div>
             <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-
-        <!-- fix for small devices only -->
-        <div class="clearfix visible-sm-block"></div>
-
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <!-- fix for small devices only -->
+    <div class="clearfix visible-sm-block"></div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
             <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
-
             <div class="info-box-content">
-              <span class="info-box-text">Mollusca</span>
-              <span class="info-box-number">760</span>
+                <span class="info-box-text">Mollusca</span>
+                <span class="info-box-number">760</span>
             </div>
             <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-        <div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box">
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box">
             <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
-
             <div class="info-box-content">
-              <span class="info-box-text">Artefak</span>
-              <span class="info-box-number">2,000</span>
+                <span class="info-box-text">Artefak</span>
+                <span class="info-box-number">2,000</span>
             </div>
             <!-- /.info-box-content -->
-          </div>
-          <!-- /.info-box -->
         </div>
-        <!-- /.col -->
-      </div>
+        <!-- /.info-box -->
+    </div>
+    <!-- /.col -->
+</div>
 </section>
 @endsection
