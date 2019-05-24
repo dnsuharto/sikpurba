923396ad04af0f8536acea9f13f3201cb26e95b2
Dzaki Nashrullah Suharto <userandroid705@gmail.com>
2019-05-24 16:21:24 +0700

    update dashboard Jumat

--- resources/views/staff_dokumentasi/index.blade.php
+++ resources/views/staff_dokumentasi/index.blade.php
@@ -3,7 +3,70 @@
 @section('body')
 
 <section class="content">
-<div class="row">
+	<div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>Batuan dan Mineral</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-hand-rock-o"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+              <p>Vertebrata dan Hominid</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-paw"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>Mollusca</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-cloud"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Artefak</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-gear"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+	<?php
+/*<div class="row">
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="ion ion-ios-paw-outline"></i></span>
@@ -61,5 +124,6 @@
         </div>
         <!-- /.col -->
       </div>
+      */?>
 </section>
 @endsection
